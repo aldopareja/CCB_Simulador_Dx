8ccfeec84d6c678f41336f3e24b1583116b130f0
aldopareja <aldo.spam@live.com>
2016-04-21 10:40:25 -0500

    I did the reshape on pesos according to this post in StackOverFlow: goo.gl/hRPwYx
    In general I needed each answer to belong to a question and an axis at the same time!, taking in mind the weight assigned to each row.

--- simulador.R
+++ simulador.R
@@ -23,3 +23,27 @@ pesos[,Eje.Pregunta.Respuesta:=gsub("[[:space:]]$","",Eje.Pregunta.Respuesta)]
 #multimplicando todos los factores que lo afecten
 pesosFinales=data.table(ejeP=character(0),preguntaP=character(0),respuestaP=character(0),
                         puntajeP=character(0))
+
+#I must reshape 'pesos' so that the order of the lines matter and gives the 
+#information about what kind of row we are using in regards of: eje, pregunta and
+#respuesta
+#
+#The way to do this is explained in this stackoverflow link:	
+# goo.gl/hRPwYx
+
+#it's not that simple (this people in SO are just genius), they use a cummulative sum
+#on the difference of a match type. The plan is getting the numerical order of the category
+#(in hierarchycal order) so that when we get a difference in hierarchy of 0 or less we get
+#a new row. Then we use a cummulative sum of this condition to number the rows. Finally we
+#add na.locf to carry on values from the past
+
+#before all that I need to get the umbrales out of the Dt
+Umbrales=pesos[Tipo=="Umbral"]
+pesos=pesos[!Tipo=="Umbral"]
+setnames(pesos,"Peso.de.prueba","p")
+#now I really got what I wanted ha!
+require(zoo)
+pesosFinales=pesos
+pesosFinales[,rowType:=match(Tipo,c("Eje","Pregunta","Respuesta"))][
+             ,newRow:=cumsum(diff(c(0,rowType))<=0)]
+pesosFinales=na.locf(dcast(pesosFinales,newRow~Tipo,value.var = c("Eje.Pregunta.Respuesta","p")))
